668d0204e5f68cd6862f4f03ddf4352db18bb0cd
Kostas Minaidis <minaidisk@gmail.com>
2018-12-04 11:24:08 +0200

    Updating JS Fundamentals Slides

---
+++ Education/TechTalentSchool/javascript/pythontutor-visuals/arrays-001.js
@@ -0,0 +1,9 @@
+let listOfNames = [ 'Abdul', 'Sofia', 'Chris' ];
+
+console.log( listOfNames );
+
+listOfNames[2] = 'Mary';
+
+console.log( listOfNames[2] );
+
+console.log( listOfNames );
---
+++ Education/TechTalentSchool/javascript/pythontutor-visuals/arrays-002.js
@@ -0,0 +1,9 @@
+let numberList = [ 1, 2, 3 ];
+
+numberList.push( 4 );
+
+numberList.pop( 4 );
+
+numberList.unshift(0);
+
+numberList.shift();
