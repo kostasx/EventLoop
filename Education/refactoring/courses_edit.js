dc44a3ac2fb78ef404f050f3c7845ef3494657a5
Kostas Minaidis <minaidisk@gmail.com>
2019-04-03 17:54:38 +0300

    Create courses_edit.js

---
+++ Education/refactoring/courses_edit.js
@@ -0,0 +1,22 @@
+## CODE REFACTORING - Case Study AfDemp - BC_EP001
+
+Code: (student) code that listens for the 'change' event on a <select> element and updates a form's content accordinly:
+
+```
+document.forms.myForm.cselect.addEventListener("change", function (e) {
+
+    let selectedIndex = document.forms.myForm.cselect.selectedIndex;
+    let courses = [
+        "Coding Bootcamp1,Java,FullTime,2020-02-01,2020-04-30",
+        "Coding Bootcamp1,Java,PartTime,2020-02-01,2020-07-30",
+        "Coding Bootcamp2,C#,FullTime,2020-05-01,2020-07-30",
+        "Coding Bootcamp2,C#,PartTime,2020-05-01,2020-10-30"
+    ]
+    document.forms.myForm.title.value = courses[selectedIndex-1].split(",")[0];
+    document.forms.myForm.stream.value = courses[selectedIndex-1].split(",")[1];
+    document.forms.myForm.type.value = courses[selectedIndex-1].split(",")[2];
+    document.forms.myForm.startdate.value = courses[selectedIndex-1].split(",")[3];
+    document.forms.myForm.enddate.value = courses[selectedIndex-1].split(",")[4];
+
+});
+```
