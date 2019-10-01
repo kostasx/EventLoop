1f3fb3017063d9981403bf202bddb7b09af97652
Kostas Minaidis <minaidisk@gmail.com>
2019-10-02 00:26:54 +0300

    Updating Angular slides

--- Education/afdemp/angular/js/afdemp-angular.js
+++ Education/afdemp/angular/js/afdemp-angular.js
@@ -1,22 +1,39 @@
 Reveal.addEventListener( "ready", handleRevealReady );
 
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+
+    // Print one page for separate slide fragments:
+    pdfSeparateFragments: false,
+
+    history: true,
+
+    dependencies: [{
+        src: 'plugin/markdown/marked.js'
+    }, {
+        src: 'plugin/markdown/markdown.js'
+    }, {
+        src: 'plugin/notes/notes.js',
+        async: true
+    }, {
+        src: 'plugin/highlight/highlight.js',
+        async: true
+    }]
+});
+
 const debugModeOn = true;
 
 function handleRevealReady( e ){
 
-    // [1] REMOVE STUDENT NOTES SLIDES IF THIS IS NOT A PDF
+    // REMOVE PRINT ONLY SLIDES
     const isPrintPdf = ( /print-pdf/gi ).test( window.location.search );
-    if ( !isPrintPdf && !debugModeOn  ){
-
-        const studentNotes = document.querySelectorAll(".student-notes");
-        studentNotes.forEach(function(studentNote){
-        
-            studentNote.remove();
-        
-        })
-        
+    if ( !isPrintPdf  ){
+    // if (!document.location.search.slice(1).match("print-pdf")) {
+        const printOnlySections = document.querySelectorAll("section.print-only");
+        Array.from(printOnlySections).forEach((section) => section.remove());
         Reveal.sync();
-
     }
 
 }
