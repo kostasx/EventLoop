21917a7b9a4625dca0beae81dfda153da9e11eac
Kostas Minaidis <minaidisk@gmail.com>
2021-10-28 01:11:25 +0200

    Added Context API React examples (WIP)

---
+++ LearnReact/examples/context-api/README.md
@@ -0,0 +1,5 @@
+#### React: Context API
+
+- [React | Context API | **Codesandbox**](https://codesandbox.io/s/github/kostasx/EventLoop/tree/master/LearnReact/examples/context-api)
+- [React | Context API | **GitHub Dev**](https://github.dev/kostasx/EventLoop/tree/master/LearnReact/examples/context-api)
+- [React | Context API | **GitHub Repo**](https://github.com/kostasx/EventLoop/tree/master/LearnReact/examples/context-api)
---
+++ LearnReact/examples/context-api/src/index.js
@@ -0,0 +1,55 @@
+import "./styles.css";
+import ReactDOM from "react-dom";
+import { createContext, useState, useContext } from "react";
+// createContext: used to create the Context.Provider and Context.Consumer Components
+// useContext: Hook for accessing the Context.Provider value
+const MyContext = createContext();
+// ALT SYNTAX:
+// const { Provider, Consumer } = MyContext;
+const ContextValue = { counter: 0 };
+console.clear();
+
+function Child() {
+  const { counter } = useContext(MyContext);
+  return (
+    <div className="child">
+      <h3>Child</h3>
+      <p>{counter}</p>
+      <GrandChild />
+    </div>
+  );
+}
+
+function GrandChild() {
+  return (
+    <MyContext.Consumer>
+      {(contextValue) => {
+        return (
+          <div className="grandchild">
+            <h3>GrandChild</h3>
+            <p
+              onClick={() => contextValue.setCounter(contextValue.counter + 1)}
+            >
+              {contextValue.counter}
+            </p>
+          </div>
+        );
+      }}
+    </MyContext.Consumer>
+  );
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <MyContext.Provider value={{ counter, setCounter }}>
+      <div className="parent">
+        <h3>Parent</h3>
+        <Child />
+        <Child />
+      </div>
+    </MyContext.Provider>
+  );
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
